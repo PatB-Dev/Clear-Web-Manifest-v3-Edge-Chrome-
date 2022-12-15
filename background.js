337e72186ef995d815c75de26c4af5c9409a4135
code-me-live <patricebelda@gmail.com>
2022-12-15 20:45:53 +0100

    test chatGPT

--- background.js
+++ background.js
@@ -1,32 +1,30 @@
-let removeElement = false
-const onIcon = "./icons/PatOn.png"
-const offTicon = "./icons/PatOff.png"
+const onIcon = "./icons/PatOn.png";
+const offTicon = "./icons/PatOff.png";
+let removeElement = false;
 
-setDefaultIcon = (tabId) => {
-    removeElement = false
-    chrome.browserAction.setIcon({ path: offTicon })
+const setDefaultIcon = (tabId) => {
+    removeElement = false;
+    chrome.browserAction.setIcon({ path: offTicon });
 
-    //qaund on change de fenetre
+    // Quand on change de fenêtre
     if (tabId) {
-        chrome.tabs.sendMessage(tabId, removeElement)
+        chrome.tabs.sendMessage(tabId, { removeElement });
     }
-}
+};
 
-// when tabs get changed, setDefaultIcon is called
-chrome.tabs.onActivated.addListener((activeInfo) => setDefaultIcon(activeInfo.tabId))
+// Lorsque les onglets changent, setDefaultIcon est appelé
+chrome.tabs.onActivated.addListener((activeInfo) => setDefaultIcon(activeInfo.tabId));
 
-try {
-    chrome.action.onClicked.addListener((tab) => {
-        removeElement = !removeElement
-        chrome.action.setIcon({ path: removeElement ? onIcon : offTicon })
-        chrome.tabs.sendMessage(tab.id, removeElement)
-    })
-} catch (error) { }
+chrome.browserAction.onClicked.addListener((tab) => {
+    removeElement = !removeElement;
+    chrome.browserAction.setIcon({ path: removeElement ? onIcon : offTicon });
+    chrome.tabs.sendMessage(tab.id, { removeElement });
+});
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    status = changeInfo.status
+    const status = changeInfo.status;
     if (status !== "loading") {
-        return
+        return;
     }
-    setDefaultIcon()
-})
+    setDefaultIcon();
+});
