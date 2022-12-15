b5b6f7715f00c1ee0c253903f962f77641f7f7a4
code-me-live <patricebelda@gmail.com>
2022-12-15 20:41:34 +0100

    test chatGPT

--- front.js
+++ front.js
@@ -1,35 +1,40 @@
-//déblocage du scroll au chargement de la page
-document.getElementsByTagName('html')[0].style.setProperty("overFlow", "auto", "important")
-document.getElementsByTagName('body')[0].style.setProperty("overFlow", "auto", "important")
+// Déblocage du scroll au chargement de la page
+const htmlElement = document.getElementsByTagName('html')[0];
+const bodyElement = document.getElementsByTagName('body')[0];
 
-//quand on clique sur l'icone tous les addEventListener sont actifs
-startListener = () => {
-    document.addEventListener("mouseover", onHovered)
-    document.addEventListener("mouseout", onHovered)
-    document.addEventListener("click", onRemove)
-}
+htmlElement.style.setProperty("overflow", "auto", "important");
+bodyElement.style.setProperty("overflow", "auto", "important");
 
-onHovered = (e) => {
-    const element = e.target
-    bgColor = (e.type == "mouseover") ? "rgba(220,20,60, .7)" : "transparent;"
+// Quand on clique sur l'icone, tous les addEventListener sont actifs
+const startListener = () => {
+  document.addEventListener("mouseover", onHovered);
+  document.addEventListener("mouseout", onHovered);
+  document.addEventListener("click", onRemove);
+};
 
-    element.setAttribute("style", "background-color:" + bgColor)
-}
+// Quand on passe la souris sur un élément, on le met en surbrillance
+const onHovered = (e) => {
+  const element = e.target;
+  const bgColor = e.type === "mouseover" ? "rgba(220,20,60, .7)" : "transparent";
 
-onRemove = (e) => {
-    const parent = e.target.parentNode
-    e.preventDefault()
-    parent.removeChild(e.target)
-}
+  element.setAttribute("style", "background-color:" + bgColor);
+};
 
-// quand on éteint l'extension on supp tous les addEventListener
-finishListener = () => {
-    document.removeEventListener("mouseover", onHovered)
-    document.removeEventListener("mouseout", onHovered)
-    document.removeEventListener("click", onRemove)
-}
+// Quand on clique sur un élément, on le supprime
+const onRemove = (e) => {
+  const parent = e.target.parentNode;
+  e.preventDefault();
+  parent.removeChild(e.target);
+};
 
-//Com avec le back
+// Quand on éteint l'extension, on supprime tous les addEventListener
+const finishListener = () => {
+  document.removeEventListener("mouseover", onHovered);
+  document.removeEventListener("mouseout", onHovered);
+  document.removeEventListener("click", onRemove);
+};
+
+// Com avec le back
 chrome.runtime.onMessage.addListener((flag, sender, sendResponse) => {
-    flag ? startListener() : finishListener()
-})
+  flag ? startListener() : finishListener();
+});
